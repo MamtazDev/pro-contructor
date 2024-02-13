93499367a053881f489e6813947393e57ecb3abc
nipaaa <akternipa941@gmail.com>
2024-02-13 15:38:24 +0600

    initial setup theme

---
+++ src/components/element/AllTimeSelling3.jsx
@@ -0,0 +1,56 @@
+export default function AllTimeSelling3() {
+  return (
+    <>
+      <div className="row g-4">
+        <div className="col-sm-6 col-lg-4">
+          <div className="iconbox-style1 at-home6 border-less p-0 bgc-transparent">
+            <div className="icon before-none">
+              <span className="flaticon-cv" />
+            </div>
+            <div className="details">
+              <h4 className="title text-white mt10 mb-3">
+                Seamless Connections
+              </h4>
+              <p className="text text-white">
+                Instantly connect with pre-screened, top-tier contractors in
+                your local area.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-4">
+          <div className="iconbox-style1 at-home6 border-less p-0 bgc-transparent">
+            <div className="icon before-none">
+              <span className="flaticon-web-design" />
+            </div>
+            <div className="details">
+              <h4 className="title text-white mt10 mb-3">
+                Comprehensive Features:{" "}
+              </h4>
+              <p className="text text-white">
+                From project management tools to a secure payment system, our
+                platform has it all.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-sm-6 col-lg-4">
+          <div className="iconbox-style1 at-home6 border-less p-0 bgc-transparent">
+            <div className="icon before-none">
+              <span className="flaticon-secure" />
+            </div>
+            <div className="details">
+              <h4 className="title text-white mt10 mb-3">
+                Diverse Service Range:
+              </h4>
+              <p className="text text-white">
+                * Find contractors for every project, big or small – we cater to
+                all your needs.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/section/NeedSomething4.jsx
@@ -0,0 +1,24 @@
+import AllTimeSelling3 from "../element/AllTimeSelling3";
+
+export default function NeedSomething4() {
+  return (
+    <>
+      <section className="our-features bgc-thm2 pt60 pb35">
+        <div
+          className="container wow fadeInUp"
+          style={{ visibility: "visible", animationName: "fadeInUp" }}
+        >
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="main-title">
+                <h2 className="text-white">Why Choose ProContractorsPlus.com?</h2>
+               
+              </div>
+            </div>
+          </div>
+          <AllTimeSelling3 />
+        </div>
+      </section>
+    </>
+  );
+}
