66605ee1b7e8cae6ff504f3f6f876f6e06cebbd7
nipaaa <akternipa941@gmail.com>
2024-02-23 15:57:55 +0600

    update

--- src/components/section/About1.jsx
+++ src/components/section/About1.jsx
@@ -79,7 +79,7 @@ export default function About1() {
                     </li>
                   </ul>
                 </div>
-                <Link href="/freelancer-1" className="ud-btn btn-thm">
+                <Link href="/service-1" className="ud-btn btn-thm">
                   Find Talent
                   <i className="fal fa-arrow-right-long" />
                 </Link>
